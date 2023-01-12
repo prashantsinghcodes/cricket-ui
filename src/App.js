6e2ba9d4ef9d21fd0ae688fb09f240c4c53a9ce7
Prashant Singh <prashantsingh4787@gmail.com>
2023-01-12 16:18:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			summaryList: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"http://localhost:8080/cricket/summary")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					summaryList: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, summaryList } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+    console.log(summaryList)
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				summaryList.map((summary) => (
+				<ol key = { summary['team']['team_name'] } >
+					{ summary['team']['team_name'] } - {summary['team']['team_score']}/{summary['team']['total_wickets_fallen']} ({summary['team']['overs_played']})
+          <div>
+  
+            {summary.players.map(function(player) {
+            let string = player.player_name + " " + player.runs_scored + "(" + player.balls_faced + ")"
+            return <ol>{string}</ol>;
+          })}
+           
+          </div>
+					</ol>
+				))
+			}
+		</div>
+	);
+}
 }
 
 export default App;
